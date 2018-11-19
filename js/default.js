49ad90be7222b1b8c8974cebcbdeb065685f74ea
subaru0925 <38637442+subaru0925@users.noreply.github.com>
2018-11-19 18:16:39 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -38,3 +38,50 @@ for(key in player){
  li.textContent=key+':'+player[key];
  document.getElementById('object').appendChild(li);
 }
+
+
+unction getFileName(){
+ return window.location.href.split('/').pop();
+}
+
+var var filename=getFileName();
+var opt;
+if(filename==='other.html'){
+ opt=document.querySelector('option[value="other.html"]');
+}else{
+ opt=document.querySelector('option[value="index.html"]');
+}
+opt.selected=true;
+
+document.getElmentByI('form').select.onchange=function(){
+  location.hre=document.getElementById('form').select.value;
+}
+
+var separate_time=function(time){
+ var sec=Math.floor((time/1000)%60);
+ var min=Math.loor((time/1000/60)%60);
+ var hours=Math.floor((time/1000/60/60)%24);
+ var days=Math.loor(time/1000/60/60/24);
+ return[sec,min,hours,days]
+}
+var update=function(){
+ var now=new Date();
+ var target=new Date(2020,7,24,0,0,0,0);
+ var diff=target.getTime()-now.getTime();
+ var counter=separate_time(diff);
+ document.getElementById('countdown').textContent=
+document.getElmentByI('form').select.onchange=function(){
+  location.hre=document.getElementById('form').select.value;
+}
+  '東京オリンピックまであと　'+
+  counter[3]+'日'+
+  counter[2]+'時間'+
+  counter[1]+'分'+
+  counter[0]+'秒';
+ refresh();
+}
+
+var refresh=function(){
+ setTimeout(update,1000);
+}
+update();
